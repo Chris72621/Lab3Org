8c254d872563eeef309b69f5a6d041516013b082
Christian Rodriguez <christianrodriguez@Christians-MacBook-Air-7.local>
2024-03-18 11:45:34 -0600

    updated getrindex

--- riscv.c
+++ riscv.c
@@ -37,26 +37,86 @@ void init_memory_elements(void) {
   r[2] = (uintptr_t)&mem[MEM_SIZE];
 }
 
-
-// ADD X# WHEN EVER NEEDED SP 
-int get_r_index(const char *reg_name) {
-    if (str_cmp(reg_name, "X0") == 0) return 0;
-    else if (str_cmp(reg_name, "X1") == 0) return 1;
-    else if (str_cmp(reg_name, "X7") == 0) return 7;
-    else if (str_cmp(reg_name, "RA") == 0) return 31; 
-    else if (str_cmp(reg_name, "SP") == 0) return 2;
-    else {
-        // IF NOT A REG THEN ITS A IMM
-        int imm = atoi(reg_name);
-        if(imm == 0) {
-            return -1;
-        }  else {
-            return imm;
-        }
-    }
+int is_digit(char character) {
+    return character >= '0' && character <= '9';
 }
-// WE CAN PUT OUR INSTRUCTIONS METHODS HERE
-/* Methods for Instructions*/
+
+// COVERS ACTUAL REGISTER NAMES
+int get_r_index_extended(const char *reg_name) {
+    int result = 0;
+    int i = 0;
+
+    // Finds position of X, 
+    while (reg_name[i] != '\0' && !is_digit(reg_name[i])) {
+        i++;
+    }   
+
+    // Finds the digit and converts to int 
+    while (reg_name[i] != '\0' && is_digit(reg_name[i])) {
+        result = result * 10 + (reg_name[i] - '0'); 
+        i++;
+    }
+
+    // if i == 0 then only number will be found meaning that its an imm
+    // Otherwise, will return the register number
+    return result;
+}
+
+/*
+Register ABI Name Description Saver
+x0 zero Zero constant —
+x1 ra Return address Caller
+x2 sp Stack pointer —
+x3 gp Global pointer —
+x4 tp Thread pointer Callee
+x5 t0-t2 Temporaries Caller
+x8 s0 / fp Saved / frame pointer Callee
+x9 s1 Saved register Callee
+x10-x11 a0-a1 Fn args/return values Caller
+x12-x17 a2-a7 Fn args Caller
+x18-x27 s2-s11 Saved registers Callee
+x28-x31 t3-t6 
+*/
+// COVERS NAME TO THE REGISTERS
+int get_r_index(const char *reg_name) {
+    if (str_cmp(reg_name, "zero") == 0) return 0;
+    else if (str_cmp(reg_name, "RA") == 0)  return 1; 
+    else if (str_cmp(reg_name, "SP") == 0)  return 2; 
+    else if (str_cmp(reg_name, "GP") == 0)  return 3; 
+    else if (str_cmp(reg_name, "TP") == 0)  return 4; 
+    else if (str_cmp(reg_name, "T0") == 0)  return 5; 
+    else if (str_cmp(reg_name, "T1") == 0)  return 6; 
+    else if (str_cmp(reg_name, "T2") == 0)  return 7; 
+    else if (str_cmp(reg_name, "S0") == 0)  return 8; 
+    else if (str_cmp(reg_name, "S1") == 0)  return 9; 
+    else if (str_cmp(reg_name, "A0") == 0)  return 10; 
+    else if (str_cmp(reg_name, "A1") == 0)  return 11; 
+    else if (str_cmp(reg_name, "A2") == 0)  return 12; 
+    else if (str_cmp(reg_name, "A3") == 0)  return 13; 
+    else if (str_cmp(reg_name, "A4") == 0)  return 14; 
+    else if (str_cmp(reg_name, "A5") == 0)  return 15; 
+    else if (str_cmp(reg_name, "A6") == 0)  return 16; 
+    else if (str_cmp(reg_name, "A7") == 0)  return 17; 
+    else if (str_cmp(reg_name, "S2") == 0)  return 18; 
+    else if (str_cmp(reg_name, "S3") == 0)  return 19; 
+    else if (str_cmp(reg_name, "S4") == 0)  return 20; 
+    else if (str_cmp(reg_name, "S5") == 0)  return 21; 
+    else if (str_cmp(reg_name, "S6") == 0)  return 22; 
+    else if (str_cmp(reg_name, "S7") == 0)  return 23; 
+    else if (str_cmp(reg_name, "S8") == 0)  return 24; 
+    else if (str_cmp(reg_name, "S9") == 0)  return 25; 
+    else if (str_cmp(reg_name, "S10") == 0) return 26; 
+    else if (str_cmp(reg_name, "S11") == 0) return 27;
+    else if (str_cmp(reg_name, "T3") == 0)  return 28; 
+    else if (str_cmp(reg_name, "T4") == 0)  return 29; 
+    else if (str_cmp(reg_name, "T5") == 0)  return 30; 
+    else if (str_cmp(reg_name, "T6") == 0)  return 31; 
+    else {
+        return get_r_index_extended(reg_name);
+    }
+    return -1;
+} 
+
 
 /*1) Loads CHRIS*/
 // LB X7,1000(X5)
@@ -108,14 +168,181 @@ int LW (char *instr) {
 }
 
   /*2)	Stores MARY */
-
   /*3)	Arithmetic SHARED */
+int ADD (char *instr) {
+    int rd, rs1, rs2;
+    char *token;
+
+    token = tokenize(instr," ,");
+    rd = get_r_index(token);
+
+    token = tokenize(NULL, " ,");
+    rs1 = get_r_index(token);
+
+    token = tokenize(NULL, " ,");
+    rs2 = get_r_index(token);
+
+    if (rd == -1) {
+        return 0;
+    }
+
+    if (rs1 == -1 || rs2 == -1) {
+        return 0;
+    }
+
+    r[rd] = r[rs1] + r[rs2];
+
+    return 1;
+}
+
+int ADDI (char *instr) {
+    int rd, rs1, imm;
+    char *token;
+
+    token = tokenize(instr," ,");
+    rd = get_r_index(token);
+
+    token = tokenize(NULL, " ,");
+    rs1 = get_r_index(token);
+
+    token = tokenize(NULL, " ,");
+    imm = get_r_index(token);
+
+    if (rd == -1) {
+        return 0;
+    }
+
+    if (rs1 == -1 || imm == -1) {
+        return 0;
+    }
+
+    r[rd] = r[rs1] + imm;
+
+    return 1;
+}
 
   /*4)	Bitwise operators MARY*/
 
   /*5)	Pseudo-instructions CHRIS*/
+  // MV RD,RS   MV A2,X0   ADDI RD,RS,IMM
+int MV (char *instr) {
+    int rd, rs;
+    char *token;
 
+    token = tokenize(instr," ,");
+    rd = get_r_index(token);
+
+    token = tokenize(NULL," ,");
+    rs = get_r_index(token);
+
+    if (rd == -1 || rs == -1) {
+        return 0;
+    }
+
+    r[rd] = r[rs];
+
+    return 1;
+}
+
+// LI RD,IMM   LI A1,6   ADDI RD,X0,IMM
+int LI (char *instr) {
+    int rd, imm;
+    char *token;
+
+    token = tokenize(instr," ,");
+    rd = get_r_index(token);
+
+    token = tokenize(NULL," ,");
+    imm = get_r_index(token);
+
+    if (rd == -1 || imm == -1) {
+        return 0;
+    }
+
+    r[rd] = imm;
+
+    return 1;
+}
+
+int NED (char *instr) {
+    int rd, rs;
+    char *token;
+
+    token = tokenize(instr," ,");
+    rd = get_r_index(token);
+
+    token = tokenize(NULL," ,");
+    rs = get_r_index(token);
+
+    if (rs == -1 || rd == -1) {
+        return 0;
+    }
+
+    r[rd] = r[rs];
+
+    return 1;
+}
+
+// NOT T0,T1
+int NOT(char *instr) { 
+    int rd, rs;
+    char *token;
+
+    // Tokenize to extract destination register (RD)
+    token = tokenize(instr, " ,");
+    rd = get_r_index(token);
+
+    // Tokenize again to extract source register (RS)
+    token = tokenize(NULL, " ,");
+    rs = get_r_index(token);
+
+    if (rd == -1 || rs == -1) {
+        return 0;
+    }
+
+    r[rd] = ~r[rs];
+
+    return 1;
+}
   /*6)	Jump offset: CHRIS*/
+int JAL (char *instr) {
+    int rd, imm;
+    char *token;
+
+    token = tokenize(instr, " ,");
+    rd = get_r_index(token);
+
+    token = tokenize(instr, " ,");
+    imm = get_r_index(token);
+
+    if (rd == -1 || imm == -1) {
+        return 0;
+    }
+
+    int address = pc + imm;
+    r[rd] = pc + 4;
+    pc = address;
+
+    return 1; 
+}
+
+int J (char *instr) {
+    int imm;
+    char *token;
+
+    token = tokenize(instr, " ,");
+    imm = get_r_index(token);
+
+    if (imm == -1) {
+        return 0;
+    }
+
+    int address = pc + imm;
+    pc = address;
+    return 1; 
+}
+
+
 
   /*7)	Jump offset + Reg: MARY*/
 
@@ -200,6 +427,7 @@ int main(int argc, char **argv) {
     close_file();
     return 1;
   }
+
 
   /* --- Your code starts here. --- */
 
